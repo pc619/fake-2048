f97e659364636a1fc4c60473883a12f46218f3aa
pc619 <pc619@ic.ac.uk>
2020-08-21 00:05:09 +0400

    Empty Functional Files

--- static/board2048.js
+++ static/board2048.js
@@ -13,165 +13,97 @@ Board.zeroBoard = () => [
 
 const condense = filter((x) => x !== 0);
 
-const combineReducer = function (acc, arg) {
-    if (arg === acc.hold) {
-        return {"hold": 0, "array": acc.array.concat(arg + 1)};
-    }
-    return {"hold": arg, "array": acc.array.concat(acc.hold)};
-};
+// ------------ TASK 1 : CREATING THE BOARD MOVEMENTS  ------------ //
 
-const combine = function (row) {
-    const initial = {"hold": 0, "array": []};
-    const reduced = row.reduce(combineReducer, initial);
-    return condense(reduced.array.concat(reduced.hold));
-};
+// Tips:
+// - Start with Board.left and try to derive the rest from those!
+// - Play the game a bit and try to understand what is happening.
+// - Try to understand what happens in one row and map it across
+//   all the rows in the array!
+// - Don't forget you might need to create other functions for that one to work!
 
-const pad = (row) => [0, 0, 0, 0].map((ignore, i) => row[i] || 0);
+// The condense function is your first and only clue! Think about what it does.
 
-const rowLeft = compose(pad, combine, condense);
+const condense = filter((x) => x !== 0);
 
-const flipH = map(List.reverse);
-const flipV = compose(List.transpose, flipH, List.transpose);
+Board.left = 
 
-Board.left = map(rowLeft);
-Board.right = compose(flipH, Board.left, flipH);
-Board.up = compose(List.transpose, Board.left, List.transpose);
-Board.down = compose(flipV, Board.up, flipV);
+Board.right =
 
-Board.left.toString = () => "Left";
-Board.right.toString = () => "Right";
-Board.up.toString = () => "Up";
-Board.down.toString = () => "Down";
+Board.up =
 
-// Board.score = function (board) {
-//     let total = 0;
+Board.down =
 
-//     board.forEach(function (row) {
-//         row.forEach(function (tile) {
-//             if (tile !== 0) {
-//                 total += 2 ** tile;
-//             }
-//         });
-//     });
+// ------------ TASK 2 : CALCULATING THE SCORE  ------------ //
 
-//     return total;
-// };
+// Calculating the score of the board (sum of all the values on the board)
 
-const flatten = (board) => board.flat();
-const powerOfTwo = (x) => 2 ** x;
-const plus = (a, x) => a + x;
-const sum = (list) => list.reduce(plus, 0);
+Board.score =
 
-Board.score = pipe(
-    flatten,
-    condense,
-    map(powerOfTwo),
-    sum
-);
+// Check if a board has legal movements (it should return false if
+// all possible movements will return the same board.)
 
-Board.equals = (board1, board2) => board1.every(
-    (row1, i) => row1.every((tile, j) => tile === board2[i][j])
-);
 
-const isNoOp = (move) => (board) => Board.equals(move(board), board);
+// ------------ TASK 3 : LEGAL MOVES  ------------ //
 
-Board.hasLegalMoves = (board) => moveList.map(isNoOp).some(
-    (noOpTests) => !noOpTests(board)
-);
 
-const unflatten = function (flatboard, returnArray = []) {
-    if (flatboard.length > 4) {
-        const [a, b, c, d, ...rest] = flatboard;
-        return unflatten(rest, returnArray.concat([[a, b, c, d]]));
-    }
-    return returnArray.concat([flatboard]);
-};
+Board.hasLegalMoves =
 
-const freeTilesRow = (row) => row.filter((tile) => tile === 0).length;
+// Find the number of empty tiles in the board (Board.freeTiles)
 
-Board.freeTiles = pipe(
-    map(freeTilesRow),
-    sum
-);
+// ------------ TASK 4 : FREE TILES  ------------ //
+
+Board.freeTiles = 
+
+
+// ------------ TASK 5 : ADDING RANDOM TILE TO BOARD  ------------ //
+
+// Then, create a function (Board.withNewTile) that places a tile
+// of a certain value (set as parameters) in one of the empty a freeTileN
+// also set as a parameter.
 
 Board.withNewTile = function (board, freeTileN, value) {
-    const freeTiles = Board.freeTiles(board);
-    const reducer = (a, x) => (
-        x === 0
-        ? (
-            a.count === freeTileN % freeTiles
-            ? {"count": a.count + 1, "array": a.array.concat(value)}
-            : {"count": a.count + 1, "array": a.array.concat(0)}
-        )
-        : {"count": a.count, "array": a.array.concat(x)}
-    );
 
-    const flatBoard = board.flat();
-    const initialValue = {"count": 0, "array": []};
-    const modified = flatBoard.reduce(reducer, initialValue).array;
-    return unflatten(modified);
 };
 
-// /**
-//  * returns a new board which is a copy of board,
-//  * with the free tile indicated replaced with value.
-//  *
-//  * This function is implemented in quite an imperative way.
-//  */
-// Board.withNewTile = function (board, freeTile, value) {
-//     // first identify the row to edit.
-//     let rowIndex;
-//     board.forEach(function (row, i) {
-//         if (rowIndex !== undefined) {
-//             return;
-//         }
-//         const freeTilesInRow = freeTilesRow(row);
-//         if (freeTile < freeTilesInRow) {
-//             rowIndex = i;
-//         } else {
-//             freeTile -= freeTilesInRow;
-//         }
-//     });
+// Finally, create a function (Board.withRandomNewTile), which takes in
+// a board as a parameter and then implements the Board.withNewTile function
+// by calculating a random number between 1 and 2 and an arbitrary empty tile.
 
-//     //clone row, and edit marked element.
-//     const newRow = board[rowIndex].slice();
-//     newRow.forEach(function (tile, i) {
-//         if (tile === 0) {
-//             if (freeTile === 0) {
-//                 newRow[i] = value;
-//             }
-//             freeTile -= 1;
-//         }
-//     });
-
-//     // clone original board, insert new row and return;
-//     const newBoard = board.slice();
-//     newBoard[rowIndex] = newRow;
-//     return newBoard;
-// };
 
 Board.withRandomNewTile = function (board) {
-    const freeTiles = Board.freeTiles(board);
-    if (freeTiles === 0) {
-        return board;
-    }
-    return Board.withNewTile(
-        board,
-        Math.floor(Math.random() * freeTiles),
-        Math.floor(1 + Math.random() * 2)
-    );
+    // You can add anything else to the function that you deem necessary.
+    // Eg. what if there are no empty tiles? What should be returned then?
+    return Board.withNewTile();
 };
 
-const start = [
-    [1, 1, 2, 2],
-    [1, 0, 0, 1],
-    [0, 0, 1, 1],
-    [0, 2, 2, 3]
-];
+// ------------ TASK 6 (OPTIONAL) : MOVEMENT OF EACH TILE  ------------ //
 
-console.log(
-    start,
-    Board.score(start)
-);
+
+// Finally, this is the optional section that will allow you to potentially
+// create the animations.
+
+// Remember, if you want to create animations you will have to make some edits
+// in the ui20148.js module and you will need to do some research.
+
+// First, create the function Board.tileMovementLeft, this function must return
+// a board of the same dimensions as the original (4 by 4) but each cell will
+// contain the amount of tiles that specific cell moves (don't calculate it for
+// 0 values).
+
+// Remember, it must also consider whether the cell merges or not!
+
+
+Board.tileMovementLeft =
+
+// Once you have completed the Board.tileMovementLeft, you can derive the
+// following ones.
+
+Board.tileMovementRight =
+
+Board.tileMovementUp =
+
+Board.tileMovementDown =
+
 
 export default Object.freeze(Board);
